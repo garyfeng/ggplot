dd1898c9cf5038bc4e26a06cd5a970a690041562
Gary Feng <gfeng@ets.org>
2016-04-24 11:26:20 -0400

    adding line and rect methods
    Efficiency comparision between Bar and Rect methods
    I've built in a parameter method={"bar(default)"|"rect"|"line"} in the
    aes(). The following tests the time ... not much difference with 500
    bars; all around 1.2-1.4 seconds.
    
    With n=5,000 blocks to draw, it takes about 13 secs for both. Line is
    about 10% faster than bar. Again, the difference is minimal.

--- ggplot/geoms/geom_segment.py
+++ ggplot/geoms/geom_segment.py
@@ -5,14 +5,17 @@
 
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
+import itertools
 import numpy as np
 import pandas as pd
 import matplotlib.cbook as cbook
+from matplotlib.patches import *
+from matplotlib.lines import *
 
 from .geom import geom
 from ggplot.utils import is_string
 from ggplot.utils import is_categorical
-
+import warnings
 
 class geom_segment(geom):
 
@@ -28,12 +31,14 @@ class geom_segment(geom):
     #                'linetype': 'solid', 'size': 1.0, 'weight': None, 'y': None, 'width' : None}
     # REQUIRED_AES = {'x'}
     # DEFAULT_PARAMS = {'stat': 'bin', 'position': 'stack'}
-    DEFAULT_AES = {'alpha': None, 'color': "black", 'fill': '#333333',
-                   'linetype': 'solid', 'size': 0.5, 'weight': None, 'y': None, 'width' : None}
+    DEFAULT_AES = {'alpha': None, 'color': "black",
+                    'fill': '#333333', 'barwidth':2, 'method':"bar",
+                    'linetype': 'solid', 'size': 0.5,
+                    'weight': None, 'y': None, 'width' : None}
     REQUIRED_AES = {'x', 'y', 'xend'}
     DEFAULT_PARAMS = {'stat': 'identity', 'position': 'identity'}
 
-    _extra_requires = {'y', 'width'}
+    _extra_requires = {'y', 'yend', 'width', 'barwidth', 'method'}
     _aes_renames = {'linetype': 'linestyle', 'size': 'linewidth',
                     'fill': 'color', 'color': 'edgecolor'}
     # NOTE: Currently, geom_bar does not support mapping
@@ -44,17 +49,10 @@ class geom_segment(geom):
     def __init__(self, *args, **kwargs):
         # TODO: Change self.__class__ to geom_segment
         super(geom_segment, self).__init__(*args, **kwargs)
-        # self.bottom = None
         self.ax = None
 
 
     def _plot_unit(self, pinfo, ax):
-        categorical = is_categorical(pinfo['y'])
-        # TODO:
-        # if y is categorical:
-        #   make up y coordinates, adjust the barwidth etc
-        #   do not print y ticks or the y axis
-
         # not using weight
         pinfo.pop('weight')
 
@@ -62,6 +60,41 @@ class geom_segment(geom):
         x = np.asarray(pinfo.pop('x'))
         xend = np.asarray(pinfo.pop('xend'))
 
+        # bar width with as a constant or as an aes parameter
+        barwidth = np.ones(len(x))*2
+        if "barwidth" in pinfo:
+            barwidth = np.ones(len(x)) * np.asarray(pinfo.pop('barwidth'))
+        maxBarWidth = np.max(barwidth)
+
+        categoricalY = is_categorical(pinfo['y'])
+        # TODO:
+        # if y is categorical:
+        #   make up y coordinates, adjust the barwidth etc
+        #   do not print y ticks or the y axis
+        y = np.asarray(pinfo.pop('y'))
+
+        #warnings.warn(str(len(y)))
+
+        if categoricalY:
+            y = pd.Series(y, dtype="category")
+            y.cat.categories = range(y.cat.categories.shape[0])
+            y = np.asarray(y) * max(maxBarWidth,1 )
+            # y = np.asarray(range(len(x))) * (maxBarWidth +1)
+        else:
+            # plot horizontal bars around y; +/- half width
+            y = y - barwidth/2
+
+        #warnings.warn(str(y))
+
+        if not "yend" in pinfo:
+            yend = y + barwidth/2
+        else:
+            yend = np.asarray(pinfo.pop('yend')) + barwidth/2
+
+        w = xend - x
+        h = yend - y
+
+        # width is the linewidth
         width_elem = pinfo.pop('width')
         # If width is unspecified, default is an array of 1's
         if width_elem is None:
@@ -69,22 +102,33 @@ class geom_segment(geom):
         else :
             width = np.array(width_elem)
 
-        if not "barwidth" in pinfo:
-            barwidth = 2
-        else:
-            barwidth = np.asarray(pinfo.pop('barwidth'))
-
-        # plot horizontal bars around y; +/- half width
-        y = np.asarray(pinfo.pop('y')) - barwidth/2
-
-        if not "yend" in pinfo:
-            yend = y + barwidth/2
-        else:
-            yend = np.asarray(pinfo.pop('yend'))
-
-        w = xend - x
-        h = yend - y
-
         self.ax = ax
-        ax.bar(left=x, height=h, width=w, bottom=y, **pinfo)
+        # ax.bar(left=x, height=barwidth, width=w, bottom=y, **pinfo)
+
+        # consider using rect instead of bar
+        # matplotlib.patches.Rectangle(xy, width, height, angle=0.0, **kwargs)
+        # http://matplotlib.org/api/patches_api.html#matplotlib.patches.Rectangle
+        # ax.add_patch(Rectangle((someX - .1, someY - .1), 0.2, 0.2, fill=True, alpha=1))
+
+        method = pinfo.pop('method').lower()
+        if method == "rect":
+            # patches.Rectangle() does not take vectors; need to iterate
+            # use izip for efficient iteration; don't really need enumerate
+            for ix, iy, ib, iw in itertools.izip(x,y,barwidth,w):
+                ax.add_patch(Rectangle(xy = (ix,iy), \
+                        height=ib, width=iw, fill=True, **pinfo))
+        elif method == "line":
+            # note that linewidth is already set with the `size` aes()
+            # and Line2D draws a continueous line for (x,y)
+            # we need to break it apart
+            # get rid of linewide in the aes(), because we will set here.
+            pinfo.pop('linewidth')
+            c = pinfo.pop('color')
+            for ix, iy, ib, iw in itertools.izip(x,y,barwidth,w):
+                # ax.add_line(Line2D((ix, ix+iw), (iy, iy), lw=ib, **pinfo))
+                ax.add_line(Line2D((ix, ix+iw), (iy, iy), lw=ib, color=c))
+
+        else:
+            ax.bar(left=x, height=barwidth, width=w, bottom=y, **pinfo)
+
         ax.autoscale()
